069a41382a84298d0b53fb45ba3f2a078758d3b4
Mason Shewman <mason@emberstories.com>
2026-03-16 11:51:35 -0400

    type1: scale grid width down to 175%

--- src/pages/ScrollTestPage.jsx
+++ src/pages/ScrollTestPage.jsx
@@ -59,7 +59,7 @@ function applyAnimation(grid, animationType, triggerOverrides = {}) {
       grid.style.setProperty('--perspective', '1000px')
       grid.style.setProperty('--grid-inner-scale', '0.5')
       grid.style.setProperty('--grid-columns', '8')
-      grid.style.setProperty('--grid-width', '250%')
+      grid.style.setProperty('--grid-width', '175%')
       timeline
         .set(gridWrap, { rotationY: 25 })
         .set(gridItems, { z: () => gsap.utils.random(-1600, 200) })
